7ef3e753008242f8a53d7f3690730386a2f9669d
David Li <davidli012345@gmail.com>
2022-12-26 04:41:53 +0000

    fix: basic data scrapping complete

--- scripts/get_pokemon.py
+++ scripts/get_pokemon.py
@@ -47,12 +47,131 @@ def adjust_pokemon_name(pokemon_name: str):
     if pokemon_name == "mr-mime":
         return "Mr._Mime"
 
+    if pokemon_name == "mime-jr":
+        return "Mime_Jr."
+
+    if pokemon_name == "deoxys-normal":
+        return "Deoxys"
+    if pokemon_name == "wormadam-plant":
+        return "Wormadam"
+    if pokemon_name == "giratina-altered":
+        return "Giratina"
+    if pokemon_name == "flabebe":
+        return "Flabébé"
+    if pokemon_name == "meowstic-male":
+        return "Meowstic"
+    if pokemon_name == "Aegislash":
+        return "Aegislash"
+
+    if pokemon_name == "type-null":
+        return "Type:_Null"
+    if pokemon_name == "minior-red-meteor":
+        return "Minior"
+    if pokemon_name == "mimikyu-disguised":
+        return "Mimikyu"
+    if pokemon_name == "jangmo-o":
+        return "Jangmo-o"
+    if pokemon_name == "hakamo-o":
+        return "Hakamo-o"
+    if pokemon_name == "kommo-o":
+        return "Kommo-o"
+    if pokemon_name == "tapu-koko":
+        return "Tapu_Koko"
+
+    if pokemon_name == "tapu-lele":
+        return "Tapu_Lele"
+
+    if pokemon_name == "tapu-bulu":
+        return "Tapu_Bulu"
+
+    if pokemon_name == "tapu-fini":
+        return "Tapu_Fini"
+
+    if pokemon_name == "toxtricity-amped":
+        return "Toxtricity"
+    if pokemon_name == "sirfetchd":
+        return "Sirfetch'd"
+
+    if pokemon_name == "mr-rime":
+        return "Mr._Rime"
+
+    if pokemon_name == "eiscue-ice":
+        return "Eiscue"
+
+    if pokemon_name == "indeedee-male":
+        return "Indeedee"
+    if pokemon_name == "morpeko-full-belly":
+        return "Morpeko"
+
+    if pokemon_name == "urshifu-single-strike":
+        return "Urshifu"
+    if pokemon_name == "zacian-hero":
+        return "Zacian"
+
+    if pokemon_name == "basculegion-male":
+        return "Basculegion"
+
+    if pokemon_name == "enamorus-incarnate":
+        return "Enamorus"
+    
+    if pokemon_name == "deoxys-attack":
+        return None
+    if pokemon_name == "deoxys-defense":
+        return None
+    if pokemon_name == "deoxys-speed":
+        return None
+    if pokemon_name == "wormadam-sandy":
+        return None
+    if pokemon_name == "wormadam-trash":
+        return None
+
+    if pokemon_name == "shaymin-sky":
+        return "shaymin"
+
+    if pokemon_name == "giratina-origin":
+        return None
+    if pokemon_name == "rotom-heat":
+        return "rotom"
+    if pokemon_name == "rotom-wash":
+        return None
+    if pokemon_name == "rotom-frost":
+        return None
+    if pokemon_name == "rotom-fan":
+        return None
+    if pokemon_name == "rotom-mow":
+        return None
+    if pokemon_name == "castform-sunny":
+        return "castform"
+    if pokemon_name == "castform-rainy":
+        return None
+    if pokemon_name == "castform-snowy":
+        return None
+
+    if pokemon_name == "basculin-blue-striped":
+        return "basculin"
+    if pokemon_name == "darmanitan-zen":
+        return "darmanitan"
+
+    if pokemon_name == "meloetta-pirouette":
+        return "meloetta"
+
+    if pokemon_name == "tornadus-therian":
+        return "Tornadus"
+    if pokemon_name == "thundurus-therian":
+        return "Thundurus"
+    if pokemon_name == "landorus-therian":
+        return "Landorus"
+    if pokemon_name == "kyurem-black":
+        return "Kyurem"
+    if pokemon_name == "kyurem-white":
+        return None
+    
     if "-" in pokemon_name:
         # capitalize first letter split by -
         adjusted_name = pokemon_name.split("-")
         adjusted_name = [name.capitalize() for name in adjusted_name]
-        adjusted_name = "-".join(adjusted_name)
-        return adjusted_name
+        # adjusted_name = "-".join(adjusted_name)
+        return None
     # capitalize first letter
     pokemon_name = pokemon_name.capitalize()
     return pokemon_name
@@ -96,6 +215,9 @@ if __name__ == "__main__":
             continue
         # adjust pokmeon name by removing everything past -
         adjusted_name = adjust_pokemon_name(pokemon["name"])
+        if adjusted_name == None:
+            print(f"Could not find image for {pokemon['name']}")
+            continue
         img_src = parse_bulbagarden_by_pokemon_name(adjusted_name)
         # save image url in pokemon
         if img_src == None:
